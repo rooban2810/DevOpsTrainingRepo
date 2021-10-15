c8d941cd177001f740cc5bd5324c555fbe299c54
rooban2810 <rooban2810@gmail.com>
2021-10-15 09:10:27 +0800

    First Test11

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using DevOpsDemo.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+namespace DevOpsDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        IPostRepository postRepository;
+        public HomeController(IPostRepository _postRepository)
+        {
+            postRepository = _postRepository;
+        }
+        public IActionResult Index()
+        {
+            var model = postRepository.GetPosts();
+            return View(model);
+        }
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+
+            return View();
+        }
+
+    
+    }
+}
